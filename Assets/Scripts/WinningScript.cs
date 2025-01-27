8d8954376c9c9b6b3a62f485e556ca5f83ea79cf
unknown <vincentxy101@gmail.com>
2025-01-27 16:04:56 -0500

    Adding screamer

--- Assets/Scripts/WinningScript.cs
+++ Assets/Scripts/WinningScript.cs
@@ -7,7 +7,7 @@ public class WinningScript : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") || collision.CompareTag("Bubble"))
+        if (collision.CompareTag("Player"))
         {
             SceneManager.LoadScene(3);
         }    
